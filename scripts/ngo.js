d56b8b05c04dfe94b287afe65a948e8c99603816
saloniparekh <suparekh01@gmail.com>
2020-05-19 18:13:39 +0530

    Add help prog form validation

--- scripts/ngo.js
+++ scripts/ngo.js
@@ -37,6 +37,7 @@ window.onload = function () {
             })
     });
 
+    //------------------------------------------------------------------------------------------------------------------------------------------------------
 
     //VIEW PROFILE JS
     document.getElementById("profile").addEventListener("click", function (e) {
@@ -70,5 +71,126 @@ window.onload = function () {
                 console.log(err);
             })
     })
+
+    //------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    //ADD HELP PROGRAMME JS
+
+    document.getElementById("prsub").addEventListener("click", function (e) {
+
+        var progname = document.getElementById("progname").value;
+        var progdesc = document.getElementById("progdesc").value;
+        var progaid = document.getElementById("progaid").value;
+        var progcity = document.getElementById("progcity").value;
+        var progaddr = document.getElementById("progaddr").value;
+    
+    
+    
+        let status = [];
+    
+    
+        if (progname.length <= 1) {
+          document.getElementById("progname").style.borderColor = "red";
+          document.getElementById("progname").value = "";
+          document.getElementById("progname").placeholder = "Please enter valid name";
+          status.push("false")
+          document.getElementById("progname").classList.add("red");
+        } else {
+          status.push("true")
+        }
+        if (progdesc.length < 1) {
+          document.getElementById("progdesc").style.borderColor = "red";
+          document.getElementById("progdesc").value = "";
+          document.getElementById("progdesc").placeholder = "Please enter valid description";
+          status.push("false")
+          document.getElementById("progdesc").classList.add("red");
+        } else {
+          status.push("true")
+        }
+        if (progaid.length <= 1) {
+          document.getElementById("progaid").style.borderColor = "red";
+          document.getElementById("progaid").value = "";
+          document.getElementById("progaid").placeholder = "Please enter valid Aid";
+          status.push("false")
+          document.getElementById("progaid").classList.add("red");
+        } else {
+          status.push("true")
+        }
+        if (progcity.length <= 1) {
+            document.getElementById("progcity").style.borderColor = "red";
+            document.getElementById("progcity").value = "";
+            document.getElementById("progcity").placeholder = "Please enter valid city";
+            status.push("false")
+            document.getElementById("progcity").classList.add("red");
+          } else {
+            status.push("true")
+          }
+          if (progaddr.length <= 1) {
+            document.getElementById("progaddr").style.borderColor = "red";
+            document.getElementById("progaddr").value = "";
+            document.getElementById("progaddr").placeholder = "Please enter valid address";
+            status.push("false")
+            document.getElementById("progaddr").classList.add("red");
+          } else {
+            status.push("true")
+          }
+        if (status.includes("false")) {
+          console.log("There was some error while validating")
+          return false
+        }
+        else {
+          console.log("Validated")
+          document.getElementById("sub").value = "Loading..."
+          let token = (localStorage.getItem("token"));
+          fetch("https://crack-corona-hack-backend.herokuapp.com/app/donation/", {
+            method: 'POST',
+            headers: new Headers({
+              'content-type': 'application/json',
+              'Authorization': `Bearer ${token}`
+            }),
+            body: JSON.stringify({
+              item_name: itemname,
+              quantity: quantity,
+              description: description
+            }),
+          })
+            .then(res => {
+              return res.json()
+            })
+            .then(res => {
+              if (res.message.id) {
+                document.getElementById("banner").style.backgroundColor = "green";
+                document.getElementById("banner").style.display = "block";
+                document.getElementById("banner").innerHTML = "You help programme has been successfully added!!";
+                document.getElementById("banner").classList.add("error");
+                document.getElementById("sub").value = "Submit";
+                setTimeout(() => {
+                  window.location.href = "donors.html";
+                }, 2500)
+              } else if (res.err == "User already exist") {
+                document.getElementById("banner").style.backgroundColor = "red";
+                document.getElementById("banner").style.display = "block";
+                document.getElementById("banner").innerHTML = "User already exist!! Try signing in."
+                document.getElementById("banner").classList.add("error");
+                document.getElementById("sub").value = "Submit"
+              } else {
+                document.getElementById("banner").style.backgroundColor = "red";
+                document.getElementById("banner").style.display = "block";
+                document.getElementById("banner").innerHTML = "Unexpected error occurred"
+                document.getElementById("banner").classList.add("error");
+                document.getElementById("sub").value = "Submit"
+              }
+            })
+            .catch(err => {
+              document.getElementById("banner").style.backgroundColor = "red";
+              document.getElementById("banner").style.display = "block";
+              document.getElementById("banner").innerHTML = "It's on us! There was some error"
+              document.getElementById("banner").classList.add("error");
+              document.getElementById("sub").value = "Submit"
+            })
+        }
+      });
+    
 
 }
